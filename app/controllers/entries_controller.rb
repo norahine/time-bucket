1282c96be1a4e044ce8044fbcac7ecefe30128d1
Natalia Baranska <baranska.n@gmail.com>
2018-02-25 17:47:52 +0100

    Entries controller and basic views

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,9 @@
 class EntriesController < ApplicationController
   before_action :authenticate_user!
+  before_action :convert_params_to_proper_format, only: [:create]
+
   def index
+    @entries= current_user.entries
   end
 
   def new
@@ -9,8 +12,14 @@ class EntriesController < ApplicationController
   end
 
   def create
-
-
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      redirect_to tracking_path, flash: { succes: "Entry was added." }
+    else
+      @activities = current_user.activities
+      flash.now[:error] = "There was an error creating the entry."
+      render "new"
+    end
   end
 
   def edit
@@ -20,5 +29,15 @@ class EntriesController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  private
+  def entry_params
+    params.require(:entry).permit(:duration, :date, :note, :activity_id)
+  end
+
+  def convert_params_to_proper_format
+    params[:entry][:date] = Date.parse(params[:entry][:date]).strftime("%Y-%m-%d")
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  after_validation :log_validation_errors, if: :errors
+
+
+  private
+
+  def log_validation_errors
+    Rails.logger.debug self.errors.full_messages.join("\n")
+  end
 end
